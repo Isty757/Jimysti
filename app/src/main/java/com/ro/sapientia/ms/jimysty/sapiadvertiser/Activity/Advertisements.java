f727afceb5eda8c9981b1c5fb0b3c9b10e4c1978
Barabas Levente Istvan <barabas.levente.w@gmail.com>
2017-11-13 11:12:07 +0200

    Recycler view

--- app/src/main/java/com/ro/sapientia/ms/jimysty/sapiadvertiser/Activity/Advertisements.java
+++ app/src/main/java/com/ro/sapientia/ms/jimysty/sapiadvertiser/Activity/Advertisements.java
@@ -2,22 +2,39 @@ package com.ro.sapientia.ms.jimysty.sapiadvertiser.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.ro.sapientia.ms.jimysty.sapiadvertiser.Adapter.MyRecyclerViewAdapter;
 import com.ro.sapientia.ms.jimysty.sapiadvertiser.BasicActivity;
 import com.ro.sapientia.ms.jimysty.sapiadvertiser.R;
 import com.ro.sapientia.ms.jimysty.sapiadvertiser.StaticMethods;
+
+import java.util.ArrayList;
 
 /**
  * Created by Drako on 06-Nov-17.
  */
 
 public class Advertisements extends BasicActivity {
+
+    MyRecyclerViewAdapter adapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,15 +43,24 @@ public class Advertisements extends BasicActivity {
         setContentView(R.layout.advertisement);
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        //adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
